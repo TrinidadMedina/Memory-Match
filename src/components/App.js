889e82686eadb359832f1ec8198cb070f6cb1543
Trinidad Medina <trinidad.medina.silva@gmail.com>
2022-04-19 01:15:17 -0400

    ocultar y mostrar

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,29 @@
-//
-// Para incluir los diferentes sets de cartas podemos _importar_ el archivo
-// JavasSript que contenga el `export` correspondiente...
-//
 import pokemon from '../data/pokemon/pokemon.js';
+
 let arrayImg =[];
-console.log("TEST");
 
 const App = () => {
   for (let i=0;i<pokemon.items.length;i++){
     arrayImg.push(pokemon.items[i].image);
   }
-  let div = document.createElement("div");
   arrayImg = arrayImg.reverse();
-  console.log(arrayImg);
-  function shuffle (){
-    for (let i=0;i<arrayImg.length;i++){
-      let ranNumLoop = arrayImg[i];
-      let randomNum = Math.floor(Math.random() * (arrayImg.length-i))+i; 
-      arrayImg[i]= arrayImg[randomNum];// cambiar de valor posición num   
-      arrayImg[randomNum] = ranNumLoop; // reposicionar valor de posición random  
-      let el = document.createElement('img');
-      el.className = "imgShow";
+  arrayImg.sort (()=> Math.random()-0.5);
+  
+  let div = document.createElement("div");
+  div.className = "cara detras";
+  for (let i=0;i<arrayImg.length;i++){ 
+    let el = document.createElement('img');
+    el.className = "visible";
+    el.setAttribute('style', 'height: 140; width: 120px;');
+    el.src = "http://chetart.com/blog/wp-content/uploads/2012/05/playing-card-back.jpg";
+    el.addEventListener("click", myFunction, false);
+    div.appendChild(el);
+    function myFunction (){
       el.src = arrayImg[i];
-      div.appendChild(el);
     }
-    console.log(arrayImg);
-    }
-  shuffle(arrayImg);
+  }
   return div;
 };
 
 export default App;
+
