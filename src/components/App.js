9af4374d4ef9c6d765b1be76839c51438d6fd3a2
MacaParedes <maka.paredes@gmail.com>
2022-04-27 12:42:49 -0400

    ultims version

--- src/components/App.js
+++ src/components/App.js
@@ -1,85 +1,92 @@
 import pokemon from '../data/pokemon/pokemon.js';
-let cardList = pokemon.items;
-const  App = {
-  
-  duplicateCards : () => {
-    for(let i in cardList){
-        cardList.push(cardList[i]); 
-        console.log(pokemon.items.length);
-    } return cardList;
+// let cardList = pokemon.items;
+let cantPickCard = 0;
+let card1="";
+let card2 = "";
+let card1Src = "";
+let finalCount=0;
+
+const App = { 
+  createCardList : () => {
+    let cardList = pokemon.items;
+    console.log(cardList);
+    return cardList;
   },
-  
-  /*createCardsImage : () => {
-    let cardListImage = [];
-    for(let i in pokemon.items){
-      cardListImage.push(pokemon.items[i].image)  
+  duplicateCards : (cardList) => {
+    for(let i in cardList){
+      cardList.push(cardList[i]); 
     }
-    return cardListImage;
-  },*/  
-  
-  shuffle : () => {
-    let shuffle = App.duplicateCards().sort (()=> Math.random()-0.5);
+    return cardList;
+  },
+  shuffle : (duplicateCardList) => {
+    let shuffle = duplicateCardList.sort (()=> Math.random()-0.5);
     return shuffle;
   },
-
-  createBoardElements : () => {
+  createBoardElements : (shuffleCardList) => {
     let div = document.createElement("div");
     div.className = "divCard";
-    for(let i in App.shuffle()){
+    for(let i in shuffleCardList){
       let miniDiv = document.createElement('div');
       miniDiv.className = "miniDiv";
-      let front = document.createElement('img');
-      front.src = pokemon.items[i].image;
-      front.className = "front";
-      front.id = pokemon.items[i].id;
-      let back = document.createElement('img');
-      back.src = "https://www.serebii.net/Toolbar/icon/gologo.png";
-      back.className = 'back';
-      miniDiv.appendChild(front);
-      miniDiv.appendChild(back);
+      miniDiv.id="miniDiv";
+      let card = document.createElement('img');
+      card.src = "https://w7.pngwing.com/pngs/324/645/png-transparent-pokemon-go-gotcha-video-game-jynx-pokeball-orange-pokemon-technology.png";
+      card.className = "card";
+      card.id=i;
+      miniDiv.appendChild(card);
       div.appendChild(miniDiv);
+      card.addEventListener("click", function() {
+        // console.log(card.currentTarget)
+        App.pickCard(card,shuffleCardList);
 
-      miniDiv.addEventListener("click", function(){ 
-        let selectCard = this
-        selectCard.classList.add('flip');
-        console.log(selectCard)});
-      //Dar vuelta la carta
-      //card.addEventListener("click", App.pickCard, false)
-    }
-
+      })}
     return div;
   },
-/*
-  pickCard : () => {
-      card.src=App.shuffle()[i];
-      console.log(card.src);
-  },*/
-}
-export default App;
 
+  pickCard:(card,shuffleCardList)=>{
+    if (cantPickCard==0){
+      for (let i in shuffleCardList){
+        if (card.id === i){
+          card.src=shuffleCardList[i].image;
+          cantPickCard++;
+          console.log(cantPickCard);
+          card1 = shuffleCardList[i];
+          card1Src = card;
+        }
+      } 
+    }
+    else if (cantPickCard==1 && card1Src.id!=shuffleCardList) 
+    {
+        for (let i in shuffleCardList){
+          if (card.id === i){
+            card.src=shuffleCardList[i].image;
+            cantPickCard++;
+            console.log(cantPickCard);
+            card2 = shuffleCardList[i];
+            if (card1.id===card2.id){
+              finalCount++
+              cantPickCard = 0;
+              console.log(finalCount);
+              setTimeout(function(){
+              if (finalCount==8){
+                finalCount=0;
+                alert("HAS GANADO");               
+                }
+              },1000);
 
-//   let div = document.createElement("div");
-//   div.className = "cara-detras";
-//   for (let i=0;i<arrayImg.length;i++){ 
-//     let el = document.createElement('img');
-//     el.className = "visible";
-//     el.setAttribute('style', 'height: 140; width: 120px;');
-//     el.src = "http://chetart.com/blog/wp-content/uploads/2012/05/playing-card-back.jpg";
-//     el.addEventListener("click", myFunction, false);
-//     div.appendChild(el);
-//     function myFunction (){
-//       el.src = arrayImg[i];
-//     }
-//   }
-//   return div;
-// };
-
-
-
-
-//  arrayImg = arrayImg.reverse();
-//   
-
-
+            } else{
+                setTimeout(function(){
+                  console.log("no es un match");
+                  card1Src.src = "https://w7.pngwing.com/pngs/324/645/png-transparent-pokemon-go-gotcha-video-game-jynx-pokeball-orange-pokemon-technology.png";
+                  card.src = "https://w7.pngwing.com/pngs/324/645/png-transparent-pokemon-go-gotcha-video-game-jynx-pokeball-orange-pokemon-technology.png";
+                  cantPickCard=0;
+                },1000);
+              }
+          }
+        }
+      }
+    }
+  }
+export default App
 
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,46 @@
-import  App from './components/App.js';
+import App from './components/App.js';
+
+let cardList = App.createCardList();
+// console.log(cardList);
+let duplicateCardList = App.duplicateCards(cardList);
+// console.log(duplicateCardList);
+let shuffleCardList = App.shuffle(duplicateCardList);
+// console.log(App.shuffle(duplicateCardList));
+let div = App.createBoardElements(shuffleCardList)
+
+const startBtn = document.getElementById("startBtn");
+
+startBtn.addEventListener("click", limitClicks);
+  
+
+let divCard = document.getElementById("divCard");
+
+function limitClicks(){
+    //boton start
+    startBtn.remove();
+    document.getElementById('gameContainer').appendChild(div);
+    
+    //boton restart
+    let button = document.createElement("button");
+    button.textContent ="Reiniciar";
+    button.id = "restart";
+    let gameBox = document.getElementById("gameContainer");
+    gameBox.appendChild(button);    
+    button.addEventListener('click', function(){
+        restart();
+    })
+  
+}
+
+  function restart(divCard){ 
+    console.log(div);
+    div.remove();
+    document.getElementById('gameContainer').appendChild(div)
+  }
+    
+       
 
 
-document.getElementById('gameContainer').appendChild(App.createBoardElements());
 
-// const card = document.getElementsByClassName('miniDiv')
-// console.log(typeof card);
-
-
-// const cartaClick = card.forEach(card => card.addEventListener('click', clickedcard));
+   
+      
