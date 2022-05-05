155360fc52ebf19de0ca98c4f301b336f738ce0b
Trinidad Medina <trinidad.medina.silva@gmail.com>
2022-05-04 22:19:00 -0400

    efecto maquina de escribir mensaje inicio

--- src/data/pokemon/pokemon.js
+++ src/data/pokemon/pokemon.js
@@ -6,7 +6,7 @@ export default {
     { id: 'eevee', image: 'https://www.serebii.net/pokemongo/pokemon/133.png', bgColor: '#339933', audio: 'https://play.pokemonshowdown.com/audio/cries/eevee.mp3'}, //sin sonido bueno
     { id: 'charizar', image: 'https://www.serebii.net/pokemongo/pokemon/066.png', bgColor: '#F05032', audio: 'audios/2.mp3' },
     { id: 'snorlax', image: 'https://www.serebii.net/pokemongo/pokemon/143.png', bgColor: '#F05032', audio: 'audios/1.mp3'},
-    { id: 'gengar', image: 'https://www.serebii.net/pokemongo/pokemon/094.png', bgColor: '#F05032', audio: 'http://play.pokemonshowdown.com/audio/cries/gengar.mp3' },//sin sonido bueno
+    { id: 'gengar', image: 'https://www.serebii.net/pokemongo/pokemon/094.png', bgColor: '#F05032', audio: 'audios/7.mp3' },//sin sonido bueno
     {id: 'bulbasaur', image: 'https://www.serebii.net/pokemongo/pokemon/001.png', bgColor: '#1572B6', audio: 'audios/9.mp3' },
     { id: 'umbreon', image: 'https://www.serebii.net/pokemongo/pokemon/197.png', bgColor: '#1572B6', audio: 'audios/7.mp3' },//sonido de ash
     //{ id: 'mewtwo', image: 'https://www.serebii.net/pokemongo/pokemon/150.png', bgColor: '#1572B6', audio: 'http://play.pokemonshowdown.com/audio/cries/mewtwo.mp3' },//sin sonido bueno
